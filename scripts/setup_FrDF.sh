f05a0ada49c39aa4708a793987ab645e9a4c5e9b
tguillemLSST <tguillem@cca020.in2p3.fr>
2024-01-30 17:11:38 +0100

    up

--- scripts/setup_FrDF.sh
+++ scripts/setup_FrDF.sh
@@ -1,29 +1,28 @@
-if [ -v BIAS_STABILITY_SETUP ]
+shared_stack_root=/cvmfs/sw.lsst.eu/linux-x86_64/lsst_distrib
+if [ -z "$1" ]
 then
-   return 0
-fi
-
-# set up the requested shared stack
-shared_stack_dir=$1
-
-LSST_DISTRIB=${shared_stack_dir}
-if [[ -f ${LSST_DISTRIB}/loadLSST-ext.bash ]]; then
-    source ${LSST_DISTRIB}/loadLSST-ext.bash
-    setup lsst_distrib
+#    # set up the most recently available weekly
+#    foo=`/usr/bin/ls -rt ${shared_stack_root} | grep w_20`
+#    weekly_version=`echo $foo | awk -F ' ' '{print $NF}'`
+    weekly_version=w_2023_49
 else
-    source ${LSST_DISTRIB}/loadLSST.bash
-    setup lsst_distrib -t $(basename "$shared_stack_dir")
+    # set up the requested weekly
+    weekly_version=$1
 fi
+shared_stack_dir=${shared_stack_root}/${weekly_version}
 
-export OMP_NUM_THREADS=1
-export NUMEXPR_MAX_THREADS=1
-export OMP_PROC_BIND=false
+source /sps/lsst/users/tguillem/Rubin/stack/w_2023_49/bias_stability/scripts/setup_base_FrDF.sh ${shared_stack_dir}
 
-package_dir=/sdf/home/j/jchiang/dev
-setup -r ${package_dir}/eo_pipe -j
-setup -r ${package_dir}/bias_stability
+version=`eups list lsst_distrib -s | sed 's/\s\+/\n/g' | grep [dw]_[0-9]*_[0-9]*`
+export WEEKLY=${version}
+export BUTLER_CONFIG=/sps/lsst/groups/FocalPlane/SLAC/run6/butler/main/
+export BIAS_STABILITY_RUN=13555
+export CALIB_RUN=${BIAS_STABILITY_RUN}
+export PAYLOAD_MODIFIER="_R33"
+export SUBSELECTION="and detector in (144..152)"
+export BIAS_STABILITY_INCOLLECTION=u/jchiang/dark${PAYLOAD_MODIFIER}_${CALIB_RUN}_${WEEKLY}
+export INSTRUMENT_CLASS_NAME=LsstCam
+export INSTRUMENT_CLASS=lsst.obs.lsst.${INSTRUMENT_CLASS_NAME}
+export INSTRUMENT_NAME=LSSTCam
 
-export BIAS_STABILITY_SETUP=true
-
-export PAYLOAD_MODIFIER=""
-export SUBSELECTION=""
+PS1="\[\033]0;\w\007\][`hostname` ${INSTRUMENT_NAME} ${version}] "
